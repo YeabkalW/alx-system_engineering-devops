f0daa29117c6777880974161033c9a3b13af2019
MSI <wyeabkal@gmail.com>
2023-06-23 14:11:47 +0300

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # a manifest that kills a process
 exec { 'pkill':
   command => 'pkill -9 -f killmenow',
-  path  => ['/usr/bin', '/usr/sbin', '/bin'],
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
 }
