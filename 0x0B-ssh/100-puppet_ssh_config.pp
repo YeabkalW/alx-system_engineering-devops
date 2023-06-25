5b3f48748eb1ef8198be3091eefeadee526bed38
MSI <wyeabkal@gmail.com>
2023-06-25 08:21:53 +0300

    done 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,13 @@
 # a manifest that manages the .ssh config file
 
-class config {
-  file { '~/.ssh/config':
-    ensure  => present,
-    content => "
-      host 54.236.26.181
-        IdentityFile = ~/.ssh/config
-        PasswordAuthentication no
-    ",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600',
+file_line { 'Turn off passwd auth':
+  path => '~/.ssh/config',
+  line => 'PasswordAuthentication on',
+  match => '^#?PasswordAuthentication',
+}
 
-  }
+file_line { 'Declare identity file':
+  path => '~/.ssh/config',
+  line => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile'
 }
