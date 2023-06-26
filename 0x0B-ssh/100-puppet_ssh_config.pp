32692d3986e3c52360521b542356581a9bcbd1a3
MSI <wyeabkal@gmail.com>
2023-06-26 09:16:58 +0300

    adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,10 +4,14 @@ file_line { 'Turn off passwd auth':
   path => '~/.ssh/config',
   line => 'PasswordAuthentication on',
   match => '^[#]+[\s]*(?i)?PasswordAuthentication[\s]+(yes|no)$',
+  replace => true,
+  append_on_no_match => true,
 }
 
 file_line { 'Declare identity file':
   path => '~/.ssh/config',
   line => 'IdentityFile ~/.ssh/school',
   match => '^[#]+[/s]*(?i)IdentityFile[/s]+~/.ssh/id_rsa$',
+  replace => true,
+  append_on_no_match => true.
 }
