de811c639701d940ba2c70224c86272d6c6c926f
MSI <wyeabkal@gmail.com>
2023-06-27 15:27:09 +0300

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,41 +1,20 @@
-class nginx_server {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
-    require   => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "# Nginx server configuration\n
-                server {
-                  listen 80 default_server;
-                  listen [::]:80 default_server;
-                  server_name _;
-
-                  location / {
-                    return 301 https://www.example.com/;
-                  }
-
-                  location = /redirect_me {
-                    return 301 https://www.example.com/;
-                  }
-
-                  location = / {
-                    return 200 'Hello World!';
-                  }
-                }\n",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx_server
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
